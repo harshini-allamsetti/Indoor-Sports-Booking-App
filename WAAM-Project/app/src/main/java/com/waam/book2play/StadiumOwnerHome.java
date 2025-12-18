da13f56e430dac28ba3aec17b603e0724578ad12
harshini-allamsetti <allamsettiharshini@gmail.com>
2025-12-18 23:45:42 +0530

    Added WAAM Android project source code

---
+++ WAAM-Project/app/src/main/java/com/waam/book2play/StadiumOwnerHome.java
@@ -0,0 +1,106 @@
+package com.waam.book2play;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class StadiumOwnerHome extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private DatabaseReference userRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stadium_owner_home);
+
+
+        toolbar = findViewById(R.id.owner_toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.oDrawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        mAuth = FirebaseAuth.getInstance();
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.openNavDrawer,
+                R.string.closeNavDrawer
+        );
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        final TextView userNameTextView = headerView.findViewById(R.id.userNameTextView);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        String userID = user.getUid();
+        userRef = FirebaseDatabase.getInstance().getReference("Users");
+
+        userRef.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User currentUser = snapshot.getValue(User.class);
+
+                if (currentUser != null) {
+                    userNameTextView.setText(currentUser.name);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(StadiumOwnerHome.this, "Something went wrong", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.oFragment_container, new MyStadiums()).commit();
+        }
+    }
+
+    //responding to click
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_ownerHome:
+                getSupportFragmentManager().beginTransaction().replace(R.id.oFragment_container, new MyStadiums()).commit();
+                break;
+            case R.id.nav_addStadium:
+                getSupportFragmentManager().beginTransaction().replace(R.id.oFragment_container, new AddStadiums()).commit();
+                break;
+            case R.id.nav_ownerLogout:
+                mAuth.signOut();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
